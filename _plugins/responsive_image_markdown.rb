216ccdfc3b6fe7e4d4c388612ac646a756e56d67
Marc Trius <derpayatz@gmail.com>
2019-02-02 17:59:25 -0600

    Added support for correct Kramdown syntax

--- _plugins/responsive_image_markdown.rb
+++ _plugins/responsive_image_markdown.rb
@@ -2,7 +2,7 @@ def markdown_responsive_image(post, payload)
   docExt = post.extname.tr('.', '')
   # only process if we deal with a markdown file
   if payload['site']['markdown_ext'].include? docExt
-    newContent = post.content.gsub(/\!\[(.*)\s*\]\(\s*(.+?)\s*(\".+?\")?\s*\)(?:\{\"?(.+?)\"?\})?/, '{% responsive_image path: \2 alt:"\1"  title:\3 class:"\4"%}')
+    newContent = post.content.gsub(/\!\[(.*)\s*\]\(\s*(.+?)\s*(\".+?\")?\s*\)(?:\{: ?\.?(.+?)\"?\})?/, '{% responsive_image path: \2 alt:"\1"  title:\3 class:"\4"%}').split(" .").join(' ')
     post.content = newContent
   end
 end
